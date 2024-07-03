ec7a199c9456f982670ec9e3ce6ed3372679218a
João Pinto <1221694@isep.ipp.pt>
2024-07-03 19:26:56 +0100

    ref #1 Fixed Dark mode button

--- src/components/DarkModeButton.jsx
+++ src/components/DarkModeButton.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import "@theme-toggles/react/css/Classic.css"
-import { Around } from "@theme-toggles/react"
+import "@theme-toggles/react/css/Around.css"
+import { Around, Within, Simple } from "@theme-toggles/react"
 
+// Classic , Lightbulb, Around, Within, Simple
 const DarkModeButton = ({ toggleDarkMode }) => {
     return (
-        <Around className='text-4xl' onClick={toggleDarkMode} duration={650} />
+        <Around className='text-4xl' onToggle={toggleDarkMode} duration={650} />
     )
 
 }
